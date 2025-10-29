637baded4725a94db0f4f8a766398e6129e6e7b7
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2025-10-29 17:26:54 -0400

    misc(dev): Update the Rust toolchain version to `nightly-2025-10-01`.

--- flake.nix
+++ flake.nix
@@ -5,10 +5,7 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    crane.url = "github:ipetkov/crane";
   };
 
   outputs = {
