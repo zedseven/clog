959dc5f3450861a7aa467687c8725aac71057c65
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2025-11-06 00:06:35 -0500

    feat(list): Print the clipboard notification regardless of the OS, then print the additional information about clipboard persistence if necessary.

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -26,20 +26,20 @@ pub fn copy_str_to_clipboard(contents: &str) -> Result<()> {
 		.set_contents(contents.to_owned())
 		.map_err(|_| anyhow!("unable to set clipboard contents"))?;
 
-	// https://github.com/alacritty/copypasta/issues/49
 	eprintln!();
+	eprintln!("Note: The output has been copied to the clipboard!");
+
+	// https://github.com/alacritty/copypasta/issues/49
 	if NON_PERSISTENT_CLIPBOARD_OSES.contains(&OS) {
 		eprintln!(
-			"Note: On this OS, the clipboard contents will be lost when the application that set \
-			 them exits."
+			"On this OS, the clipboard contents will be lost when the application that set them \
+			 exits."
 		);
 		eprintln!(
 			"To avoid this, {APPLICATION_PROPER_NAME} will wait until Enter is pressed before \
 			 exiting so that the contents can be pasted where they're needed."
 		);
 		await_user_input();
-	} else {
-		eprintln!("Note: The output has been copied to the clipboard!");
 	}
 
 	Ok(())
